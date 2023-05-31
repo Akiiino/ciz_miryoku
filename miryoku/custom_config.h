766e866492cdfe5eeb91bbe7c3e20f8f0f2332ec
Akiiino <13392695+Akiiino@users.noreply.github.com>
2023-05-31 18:16:36 +0200

    Add comment

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -3,6 +3,7 @@
 
 #define XXX &none
 
+// Move upper pinkie keys to outer pinkie keys
 #define MIRYOKU_LAYOUTMAPPING_CORNE( \
      K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
      K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
